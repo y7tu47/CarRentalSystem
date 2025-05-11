36c7c9c01ec1e859549e5672494db0a6df117702
MOHAMED\drmad <dr.mada2018@gmail.com>
2025-05-11 09:46:44 +0300

    update

--- src/app/components/cardetail/cardetail.component.ts
+++ src/app/components/cardetail/cardetail.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CrudApisService } from '../../services/crud-apis.service';
+import { ICar } from '../../model/models';
+import { Observable } from 'rxjs';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-cardetail',
-  imports: [],
+  imports: [NgIf, AsyncPipe],
   templateUrl: './cardetail.component.html',
   styleUrl: './cardetail.component.css'
 })
-export class CardetailComponent {
+export class CardetailComponent implements OnInit {
+
+  carId: number | any;
+  activatedRoute = inject(ActivatedRoute);
+  crudeApis = inject(CrudApisService);
+  baseUrl: string = "https://ca5f0d6220a74d744ce4.free.beeceptor.com/";
+  car: number| any;
+  city: string | any;
+
+
+  ngOnInit(): void {
+    this.carId = this.activatedRoute.snapshot.paramMap.get('carId');
+    // this.crudeApis.getDataById(this.baseUrl+"car/"+this.carId).subscribe(response => {
+    //   this.car = response;
+    //   this.crudeApis.getDataById(this.baseUrl+"location/"+this.car.locationId).subscribe(response => {
+    //     this.city = response.city;
+    //     console.log(response.city)
+    //   })
+      
+    // }) 
+     
+    
+  }
 
 }
--- src/app/components/carlist/carlist.component.ts
+++ src/app/components/carlist/carlist.component.ts
@@ -1,28 +1,32 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CrudApisService } from '../../services/crud-apis.service';
-import { Observable } from 'rxjs';
 import { ICar } from '../../model/models';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {MatInputModule} from '@angular/material/input';
 
 
 
 
 @Component({
   selector: 'app-carlist',
-  imports: [NgFor, RouterModule],
+  imports: [NgFor, RouterModule, NgIf, MatInputModule],
   templateUrl: './carlist.component.html',
   styleUrl: './carlist.component.css'
 })
 export class CarlistComponent implements OnInit {
   activatedRoute = inject(ActivatedRoute);
   crudeApis = inject(CrudApisService);
-  baseUrl: string = "https://ca5f0d6220a74d744ce4.free.beeceptor.com/car/";
+  baseUrl: string = "https://cab96ea27a8fc98e7ea5.free.beeceptor.com/car/";
   // carsList: Observable<any> | undefined;
   locationId: number | any;
   availableCars: ICar[] = [];
-  
+  categories: string[] = ["Recomended", "Price (lowest price)", "Top reveiwed", "Distance"];
+  selectedCategory: string | undefined;
+  sortBtnClicked: boolean = false;
+  carTypes: string[] = ["Small", "Meduim", "Larg", "SUVs", "Minivan", "premuim"];
+  carsAvailableNumber: number = 0;
 
   ngOnInit(): void {
     this.locationId = this.activatedRoute.snapshot.paramMap.get('location');
@@ -31,14 +35,23 @@ export class CarlistComponent implements OnInit {
 
   getCarsByLocationId() {
     this.crudeApis.getAllData(this.baseUrl).subscribe(response => {
+      console.log(response)
       this.availableCars = response.filter((car: ICar) => car.locationId === this.locationId);
       console.log(this.availableCars);
+      this.carsAvailableNumber = 0;
+      this.availableCars.forEach(() => {
+        this.carsAvailableNumber++;
+      })
     });
   }
 
-  // viewCarDetail(id: number) {
+  selectCategory(category: string) {
+    this.selectedCategory = category;
+  }
 
-  // }
+  isSortBtnClicked() {
+    this.sortBtnClicked = !this.sortBtnClicked;
+  }
 
 
 }
